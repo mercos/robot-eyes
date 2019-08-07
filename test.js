3118bfc76eb13080567c2c36b3fe0309717bcf81
Brendon Barreto <brendondominic@gmail.com>
2019-08-07 16:37:24 -0300

    Avoiding mutation

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const compare = require('./compare')
 const test = (url, title, options, defaultOptions) => {
   return new Promise(async (resolve, reject) => {
     try {
-      await capture(url, title, Object.assign(defaultOptions || {}, options || {}))
+      await capture(url, title, Object.assign({}, defaultOptions || {}, options || {}))
       await compare(title)
       resolve()
     } catch (e) {
