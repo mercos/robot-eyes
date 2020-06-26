08bc2e3d58c7a253f903b704ee50feae03ae976f
Brendon Barreto <brendondominic@gmail.com>
2020-06-26 19:11:35 -0300

    Fix browser hanging on error

--- capture/index.js
+++ capture/index.js
@@ -1,14 +1,12 @@
 const getConfig = require('../core/getConfig')
-const openBrowser = require('./openBrowser')
 const retry = require('../core/retry')
 const tryCapture = require('./tryCapture')
 
 const capture = async (url, testName, options = {}) => {
   const config = getConfig()
-  const browser = await openBrowser()
   const viewports = config.viewports
 
-  await retry(() => tryCapture(browser, url, testName, viewports, options, config), 3, 500)
+  await retry(() => tryCapture(url, testName, viewports, options, config), 3, 500)
 }
 
 module.exports = capture
