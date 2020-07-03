59459f292fc4e7bc41f0f6be6ac808c61458e315
Brendon Barreto <brendondominic@gmail.com>
2020-07-03 18:21:59 -0300

    On Compare

--- compare/compareHashs.js
+++ compare/compareHashs.js
@@ -1,12 +1,10 @@
 const CRC32 = require('crc-32')
 const fs = require('fs')
-const getConfig = require('../core/getConfig')
 const getFilePath = require('../core/getFilePath')
 
-const compareHashs = (fileName, viewport) => {
+const compareHashs = (config, fileName, viewport) => {
   return new Promise((resolve, reject) => {
     try {
-      const config = getConfig()
       const referenceImage = fs.readFileSync(getFilePath(config.paths.referenceImages, fileName, viewport))
       const testImage = fs.readFileSync(getFilePath(config.paths.testImages, fileName, viewport))
       const hashsAreEqual = CRC32.buf(referenceImage) === CRC32.buf(testImage)
