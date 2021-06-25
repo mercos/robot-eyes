163b32bdc2cbdb2cc196cdd84902d72f3aea8a01
Brendon Barreto <brendondominic@gmail.com>
2021-06-25 12:05:13 -0300

    Wait for resource to be available

--- commands/test/index.js
+++ commands/test/index.js
@@ -1,21 +1,39 @@
 const Mocha = require('mocha')
+const waitOn = require('wait-on')
 const cleanTestFiles = require('./cleanTestFiles')
 const getConfig = require('../../core/getConfig')
+
+const waitForResource = (config) => {
+  return waitOn({
+    interval: 1000,
+    timeout: 60000,
+    resources: [
+      config.baseURL
+    ]
+  })
+}
 
 module.exports = (testFileName, grep) => {
   return new Promise((resolve, reject) => {
     try {
       const config = getConfig()
-      cleanTestFiles()
-      const mocha = new Mocha({
-        fullTrace: true,
-        grep
-      })
-      mocha.timeout(config.timeout)
-      mocha.addFile(testFileName)
-      mocha.run(function (failures) {
-        failures ? reject(new Error(`${failures} tests failed`)) : resolve()
-      })
+
+      waitForResource(config)
+        .then(function () {
+          cleanTestFiles()
+          const mocha = new Mocha({
+            fullTrace: true,
+            grep
+          })
+          mocha.timeout(config.timeout)
+          mocha.addFile(testFileName)
+          mocha.run(function (failures) {
+            failures ? reject(new Error(`${failures} tests failed`)) : resolve()
+          })
+        })
+        .catch(function (err) {
+          reject(err)
+        })
     } catch (e) {
       reject(e)
     }
