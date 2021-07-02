cd286c19e83bceee21bd4ad12784c6d842919ee2
Brendon Barreto <brendondominic@gmail.com>
2021-07-02 12:15:24 -0300

    Configurable waitForResourceTimeout

--- commands/test/index.js
+++ commands/test/index.js
@@ -6,13 +6,11 @@ const cleanTestFiles = require('./cleanTestFiles')
 const getConfig = require('../../core/getConfig')
 const retry = require('../../core/retry')
 
-const ONE_MINUTE_IN_MILLISECONDS = 60000
-
-const waitForResource = url => {
+const waitForResource = (url, timeout) => {
   if (!url) return Promise.resolve()
 
   return retry(() => fetch(url), {
-    timeout: ONE_MINUTE_IN_MILLISECONDS,
+    timeout: timeout,
     onFailedAttempt: (e, currentAttempt, elapsedMs) => console.log(`Attempt ${currentAttempt} in ${elapsedMs / 1000}s: ${chalk.yellow(e.message)}`)
   })
 }
@@ -21,7 +19,7 @@ module.exports = (testFileName, grep) => {
   return new Promise((resolve, reject) => {
     const config = getConfig()
 
-    waitForResource(config.baseURL)
+    waitForResource(config.baseURL, config.waitForResourceTimeout)
       .then(() => {
         try {
           cleanTestFiles()
