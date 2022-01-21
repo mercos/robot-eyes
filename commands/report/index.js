a7bdf1f1aaae2d0193a12ef44b3a1229f83213a9
Caio Fernando Bertoldi Paes de Andrade <caiofbpa@icloud.com>
2022-01-21 15:15:33 -0300

    Remove dependency on report

--- commands/report/index.js
+++
@@ -1,9 +0,0 @@
-const robotEyesReport = require('robot-eyes-report')
-const getFailedTests = require('../../core/getFailedTests')
-
-const report = port => {
-  const failedTests = getFailedTests()
-  robotEyesReport(failedTests, port)
-}
-
-module.exports = report
